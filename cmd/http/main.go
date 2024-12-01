59ae05a819c5ed394a235e6cb569558907b3a09e
momoein <100838174+momoein@users.noreply.github.com>
2024-12-01 06:09:23 +0330

    Revert "Feature/profile1"

--- cmd/http/main.go
+++ cmd/http/main.go
@@ -1,38 +1,5 @@
 package main
 
-import (
-	"log"
-
-	"golizilla/config"
-	"golizilla/persistence/database"
-	"golizilla/route"
-
-	"gorm.io/gorm"
-)
-
 func main() {
-	// Load configuration
-	cfg, err := config.LoadConfig()
-	if err != nil {
-		log.Printf("Error loading config: %v", err)
-		return
-	}
-
-	// Setup database with retry logic
-	db, err := database.SetupDatabaseWithRetry(cfg, 5)
-	if err != nil {
-		log.Printf("Failed to set up the database after multiple attempts: %v", err)
-		return
-	}
-
-	// Type assertion to *gorm.DB
-	gormDB, ok := db.(*gorm.DB)
-	if !ok {
-		log.Printf("Failed to assert database to *gorm.DB")
-		return
-	}
-
-	// Start API
-	route.RunServer(cfg, gormDB)
-
+	
 }
